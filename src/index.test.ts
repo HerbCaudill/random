176b43d0c993b33db7011cd70471e3b42396218a
HerbCaudill <herb@devresults.com>
2021-06-23 16:16:23 +0200

    index.test.ts:

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,44 @@
 import { makeRandom } from '.'
 
+const N = 100000
+
 describe('makeRandom', () => {
+  describe('seed usage', () => {
+    const integerSeries = (n: number, seed?: string) => {
+      const r = makeRandom(seed)
+      return range(n)
+        .map(_ => r.integer())
+        .join()
+    }
+
+    test('same seed should give same result', () => {
+      const series1 = integerSeries(10, 'aaa')
+      const series2 = integerSeries(10, 'aaa')
+
+      expect(series1).toMatchInlineSnapshot(`"4,5,3,5,5,6,0,3,8,1"`)
+      expect(series2).toMatchInlineSnapshot(`"4,5,3,5,5,6,0,3,8,1"`)
+
+      expect(series1).toEqual(series2)
+    })
+
+    test('different seeds should give different results', () => {
+      const series1 = integerSeries(10, 'aaa')
+      const series2 = integerSeries(10, 'bbb')
+
+      expect(series1).toMatchInlineSnapshot(`"4,5,3,5,5,6,0,3,8,1"`)
+      expect(series2).toMatchInlineSnapshot(`"8,1,9,4,7,6,0,0,2,0"`)
+
+      expect(series1).not.toEqual(series2)
+    })
+
+    test('no seed should give different results every time', () => {
+      const series1 = integerSeries(10)
+      const series2 = integerSeries(10)
+
+      expect(series1).not.toEqual(series2)
+    })
+  })
+
   describe('integer', () => {
     test('1-digit numbers', () => {
       const r = makeRandom('test-seed-integer-1')
@@ -37,6 +75,28 @@ describe('makeRandom', () => {
       expect(r.integer(1000, 10000)).toMatchInlineSnapshot(`6204`)
       expect(r.integer(1000, 10000)).toMatchInlineSnapshot(`1484`)
     })
+
+    test('bins', () => {
+      const r = makeRandom('test-seed-integer-4')
+
+      const values = range(N).map(_ => r.integer(0, 10))
+
+      // results are distributed uniformly
+      expect(makeBins(values)).toMatchInlineSnapshot(`
+        Object {
+          "00": 10075,
+          "01": 10020,
+          "02": 10046,
+          "03": 10090,
+          "04": 9931,
+          "05": 10065,
+          "06": 9907,
+          "07": 9935,
+          "08": 9933,
+          "09": 9998,
+        }
+      `)
+    })
   })
 
   describe('decimal', () => {
@@ -54,34 +114,24 @@ describe('makeRandom', () => {
     test('bins', () => {
       const r = makeRandom('test-seed-decimal-2')
 
-      const values = []
-      for (let i = 0; i < 100000; i++) {
-        values.push(r.decimal(0, 10))
-      }
-
-      const bins = values.reduce((result, d) => {
-        const bin = Math.round(d)
-          .toString()
-          .padStart(2, '0')
-        return { ...result, [bin]: (result[bin] ?? 0) + 1 }
-      }, {} as Record<string, number>)
+      const values = range(N).map(_ => r.decimal(0, 10))
 
       // results are distributed uniformly
-      expect(bins).toMatchInlineSnapshot(`
-          Object {
-            "00": 5087,
-            "01": 10058,
-            "02": 9876,
-            "03": 9928,
-            "04": 10055,
-            "05": 9894,
-            "06": 9917,
-            "07": 10201,
-            "08": 10022,
-            "09": 10060,
-            "10": 4902,
-          }
-        `)
+      expect(makeBins(values)).toMatchInlineSnapshot(`
+        Object {
+          "00": 5087,
+          "01": 10058,
+          "02": 9876,
+          "03": 9928,
+          "04": 10055,
+          "05": 9894,
+          "06": 9917,
+          "07": 10201,
+          "08": 10022,
+          "09": 10060,
+          "10": 4902,
+        }
+      `)
     })
   })
 
@@ -100,33 +150,23 @@ describe('makeRandom', () => {
     test('bins', () => {
       const r = makeRandom('test-seed-normal-2')
 
-      const values = []
-      for (let i = 0; i < 100000; i++) {
-        values.push(r.normal(0, 10))
-      }
-
-      const bins = values.reduce((bins, d) => {
-        const bin = Math.round(d)
-          .toString()
-          .padStart(2, '0')
-        return { ...bins, [bin]: (bins[bin] ?? 0) + 1 }
-      }, {} as Record<string, number>)
+      const values = range(N).map(_ => r.normal(0, 10))
 
       // results are distributed normally
-      expect(bins).toMatchInlineSnapshot(`
-          Object {
-            "01": 19,
-            "02": 564,
-            "03": 6134,
-            "04": 24125,
-            "05": 38232,
-            "06": 24253,
-            "07": 6034,
-            "08": 611,
-            "09": 27,
-            "10": 1,
-          }
-        `)
+      expect(makeBins(values)).toMatchInlineSnapshot(`
+        Object {
+          "01": 19,
+          "02": 564,
+          "03": 6134,
+          "04": 24125,
+          "05": 38232,
+          "06": 24253,
+          "07": 6034,
+          "08": 611,
+          "09": 27,
+          "10": 1,
+        }
+      `)
     })
   })
 
@@ -144,18 +184,13 @@ describe('makeRandom', () => {
     test('bins', () => {
       const r = makeRandom('test-seed-coinflip-2')
 
-      const bins = { 0: 0, 1: 0 }
-      for (let i = 0; i < 100000; i++) {
-        const bin = r.coinFlip() ? 1 : 0
-        bins[bin]++
-      }
-
-      expect(bins).toMatchInlineSnapshot(`
-          Object {
-            "0": 50172,
-            "1": 49828,
-          }
-        `)
+      const values = range(N).map(_ => r.coinFlip())
+      expect(makeBins(values)).toMatchInlineSnapshot(`
+        Object {
+          "false": 50172,
+          "true": 49828,
+        }
+      `)
     })
   })
 
@@ -193,18 +228,13 @@ describe('makeRandom', () => {
     test('bins: 10%', () => {
       const r = makeRandom('test-seed-probability-3')
 
-      const bins = { 0: 0, 1: 0 }
-      for (let i = 0; i < 100000; i++) {
-        const bin = r.probability(0.1) ? 1 : 0
-        bins[bin]++
-      }
-
-      expect(bins).toMatchInlineSnapshot(`
-          Object {
-            "0": 90165,
-            "1": 9835,
-          }
-        `)
+      const values = range(N).map(_ => r.probability(0.1))
+      expect(makeBins(values)).toMatchInlineSnapshot(`
+        Object {
+          "false": 90165,
+          "true": 9835,
+        }
+      `)
     })
   })
 
@@ -212,6 +242,7 @@ describe('makeRandom', () => {
     test('from array', () => {
       const r = makeRandom('test-seed-pick-1')
       const options = [1, 2, 3]
+
       expect(r.pick(options)).toMatchInlineSnapshot(`2`)
       expect(r.pick(options)).toMatchInlineSnapshot(`3`)
       expect(r.pick(options)).toMatchInlineSnapshot(`2`)
@@ -224,38 +255,31 @@ describe('makeRandom', () => {
 
     test('from object', () => {
       const r = makeRandom('test-seed-pick-2')
-      const options = { a: 10, b: 20, c: 30 }
-      expect(r.pick(options)).toMatchInlineSnapshot(`20`)
-      expect(r.pick(options)).toMatchInlineSnapshot(`30`)
-      expect(r.pick(options)).toMatchInlineSnapshot(`10`)
-      expect(r.pick(options)).toMatchInlineSnapshot(`20`)
-      expect(r.pick(options)).toMatchInlineSnapshot(`30`)
-      expect(r.pick(options)).toMatchInlineSnapshot(`30`)
-      expect(r.pick(options)).toMatchInlineSnapshot(`10`)
-      expect(r.pick(options)).toMatchInlineSnapshot(`30`)
+      const options = { a: 'fee', b: 'fi', c: 'fo', d: 'fum' }
+
+      expect(r.pick(options)).toMatchInlineSnapshot(`"fi"`)
+      expect(r.pick(options)).toMatchInlineSnapshot(`"fo"`)
+      expect(r.pick(options)).toMatchInlineSnapshot(`"fee"`)
+      expect(r.pick(options)).toMatchInlineSnapshot(`"fi"`)
+      expect(r.pick(options)).toMatchInlineSnapshot(`"fum"`)
+      expect(r.pick(options)).toMatchInlineSnapshot(`"fum"`)
+      expect(r.pick(options)).toMatchInlineSnapshot(`"fee"`)
+      expect(r.pick(options)).toMatchInlineSnapshot(`"fo"`)
     })
 
     test('bins', () => {
       const r = makeRandom('test-seed-pick-3')
-      const options = ['fee', 'fi', 'fo', 'fum']
+      const options = [1, 2, 3, 4]
 
-      const values = []
-      for (let i = 0; i < 100000; i++) {
-        values.push(r.pick(options))
-      }
-
-      const bins = values.reduce((bins, d) => {
-        return { ...bins, [d]: (bins[d] ?? 0) + 1 }
-      }, {} as Record<string, number>)
-
-      expect(bins).toMatchInlineSnapshot(`
-          Object {
-            "fee": 24991,
-            "fi": 25015,
-            "fo": 25017,
-            "fum": 24977,
-          }
-        `)
+      const values = range(N).map(_ => r.pick(options))
+      expect(makeBins(values)).toMatchInlineSnapshot(`
+        Object {
+          "01": 24991,
+          "02": 25015,
+          "03": 25017,
+          "04": 24977,
+        }
+      `)
     })
   })
 
@@ -263,28 +287,23 @@ describe('makeRandom', () => {
     test('snapshots', () => {
       const r = makeRandom('test-seed-plusOrMinus-1a')
 
+      expect(r.plusOrMinus()).toMatchInlineSnapshot(`-1`)
+      expect(r.plusOrMinus()).toMatchInlineSnapshot(`-1`)
       expect(r.plusOrMinus()).toMatchInlineSnapshot(`1`)
       expect(r.plusOrMinus()).toMatchInlineSnapshot(`1`)
-      expect(r.plusOrMinus()).toMatchInlineSnapshot(`-1`)
-      expect(r.plusOrMinus()).toMatchInlineSnapshot(`-1`)
-      expect(r.plusOrMinus()).toMatchInlineSnapshot(`-1`)
+      expect(r.plusOrMinus()).toMatchInlineSnapshot(`1`)
     })
 
     test('bins', () => {
       const r = makeRandom('test-seed-plusOrMinus-2')
 
-      const bins = { [-1]: 0, [1]: 0 } as Record<number, number>
-      for (let i = 0; i < 100000; i++) {
-        const bin = r.plusOrMinus()
-        bins[bin]++
-      }
-
-      expect(bins).toMatchInlineSnapshot(`
-          Object {
-            "-1": 50015,
-            "1": 49985,
-          }
-        `)
+      const values = range(N).map(_ => r.plusOrMinus())
+      expect(makeBins(values)).toMatchInlineSnapshot(`
+        Object {
+          "-1": 49985,
+          "01": 50015,
+        }
+      `)
     })
   })
 
@@ -324,3 +343,18 @@ describe('makeRandom', () => {
     })
   })
 })
+
+const range = (size: number, start = 0) => new Array(size).fill(0).map((_, i) => i + start)
+
+const makeBins = (values: any[]) => {
+  return values.reduce((result, d) => {
+    const bin =
+      typeof d === 'number'
+        ? Math.round(d)
+            .toString()
+            .padStart(2, '0')
+        : d.toString()
+
+    return { ...result, [bin]: (result[bin] ?? 0) + 1 }
+  }, {} as Record<string, number>)
+}
