5300b44e76b4b9e60bc2925e7929f2bebb0d09c0
HerbCaudill <herb@devresults.com>
2021-06-23 15:26:32 +0200

    index.ts:

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,48 @@
-import seedRandom from 'seed-random';
+import seedRandom from 'seed-random'
 
-const IOTA = 0.00000000000001;
+const IOTA = 0.00000000000001
 
-export const makeRandom = (seed: string | undefined) => {
-  const randomizer = seedRandom(seed);
-  const random = () => randomizer() * (1 - IOTA) + IOTA; // ensure non-zero result
+export const makeRandom = (seed: string = new Date().getTime().toString()) => {
+  const randomizer = seedRandom(seed)
+  const random = () => randomizer() * (1 - IOTA) + IOTA // ensure non-zero result
+
   const r = {
     integer: (min: number, max: number) => Math.floor(r.decimal(min, max)),
 
-    decimal: (min: number, max: number) => random() * (max - min + 1) + min,
+    decimal: (min: number = 0, max: number = 1) => random() * (max - min) + min,
 
-    normal: (min: number, max: number, skew = 1) => {
-      let num =
-        Math.sqrt(-2.0 * Math.log(random())) *
-        Math.cos(2.0 * Math.PI * random());
-
-      num = num / 10.0 + 0.5; // Translate to 0 -> 1
-      if (num > 1 || num < 0) num = r.normal(min, max, skew); // resample between 0 and 1 if out of range
-      num = Math.pow(num, skew); // Skew
-      num *= max - min; // Stretch to fill range
-      num += min; // offset to min
-      return num;
+    normal: (min: number = 0, max: number = 1): number => {
+      const u = random()
+      const v = random()
+      let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
+      num = num / 10.0 + 0.5 // Translate to 0 -> 1
+      if (num > 1 || num < 0) return r.normal(min, max) // try again if out of bounds
+      return num * (max - min) + min
     },
 
-    coinFlip: () => r.integer(0, 1) === 0,
+    coinFlip: () => r.probability(0.5),
 
     probability: (percent: number) => random() < percent,
 
     pick: (obj: any[] | { [key: string]: any }): any => {
       if (Array.isArray(obj)) {
         // return random element of an array
-        return obj[r.integer(0, obj.length - 1)];
+        return obj[r.integer(0, obj.length)]
       } else {
         // return random property of an object
-        return obj[r.pick(Object.keys(obj))];
+        return obj[r.pick(Object.keys(obj))]
       }
     },
 
     plusOrMinus: () => r.pick([-1, 1]),
-  };
 
-  return r;
-};
+    alpha: (len: number = 5) => {
+      const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+      return '_'.repeat(len).replace(/_/g, () => r.pick(alphabet))
+    },
+  }
 
-export type Randomizer = ReturnType<typeof makeRandom>;
+  return r
+}
+
+export type Randomizer = ReturnType<typeof makeRandom>
